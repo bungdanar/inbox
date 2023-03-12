f79033c6e3285ebfaa323e02dc7081f76de28a6f
Bung Danar <danar.g.putera@gmail.com>
2023-03-12 20:51:44 +0700

    chore: Wallet provider setup

--- deploy.js
+++ deploy.js
@@ -1,1 +1,12 @@
-// deploy code will go here
+require('dotenv').config()
+
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const { interface, bytecode } = require('./compile')
+
+const provider = new HDWalletProvider(
+  process.env.ACCOUNT_MNEMONIC,
+  process.env.NETWORK_URL
+)
+
+const web3 = new Web3(provider)
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,42 @@
-// contract test code will go here
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+const { interface, bytecode } = require('../compile')
+
+const web3 = new Web3(ganache.provider())
+
+let accounts
+let inbox
+
+const INITIAL_MESSAGE = 'Hello World'
+
+beforeEach(async () => {
+  // Get list of all accounts
+  accounts = await web3.eth.getAccounts()
+
+  // Deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+    .send({ from: accounts[0], gas: '1000000' })
+})
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address)
+  })
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call()
+    assert.equal(message, INITIAL_MESSAGE)
+  })
+
+  it('can change the message', async () => {
+    const NEW_MESSAGE = 'new message'
+
+    await inbox.methods.setMessage(NEW_MESSAGE).send({
+      from: accounts[0],
+    })
+    const message = await inbox.methods.message().call()
+    assert.equal(message, NEW_MESSAGE)
+  })
+})
